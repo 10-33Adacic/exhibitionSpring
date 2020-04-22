fd01ed849213a9253927d25093e201fcd5e4cf86
Me <m.sadovskiy87@gmail.com>
2020-04-22 21:39:51 +0300

    first servlet

---
+++ src/main/java/ua/exhibition/ExhibitionApp.java
@@ -0,0 +1,11 @@
+package ua.exhibition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ExhibitionApp.class, args);
+    }
+}
