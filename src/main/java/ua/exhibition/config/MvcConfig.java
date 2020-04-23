a86a948215fd01cb139149251fbf035f27a6f053
Me <m.sadovskiy87@gmail.com>
2020-04-23 10:56:32 +0300

    add mvcConfig

---
+++ src/main/java/ua/exhibition/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package ua.exhibition.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+}
+
